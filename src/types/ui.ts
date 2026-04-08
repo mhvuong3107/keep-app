9ebbcde2800cf973e9a961e019185deca2e10989
mhvuong3107 <vuong.dv@mor.com.vn>
2026-04-08 16:50:11 +0700

    fix: tsconfig

--- src/types/ui.ts
+++ src/types/ui.ts
@@ -1,20 +1,12 @@
-// types/ui.ts
-
 import { User } from "./user";
 import { Label }  from "./label";
 
 export interface NoteUI {
   id: string;
-
   title: string;
   color: string;
-
   pinned: boolean;
   archived: boolean;
-
-  // 👥 đã map user
   collaborators: User[];
-
-  // 🏷️ đã map label
   labels: Label[];
 }
